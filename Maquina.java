9840c04b9e3be84ad0e292b9ea651719f5d4d59d
Gabriel <70419113+GabriBerp@users.noreply.github.com>
2024-04-29 09:08:07 -0300

    Update Maquina.java

--- Maquina.java
+++ Maquina.java
@@ -3,14 +3,42 @@ import java.lang.Thread;
 // Classe base para todas as máquinas
 class Maquina extends Thread {
   protected int id;
+  protected int tipo;
   protected boolean ligada;
   protected boolean emFuncionamento;
+  protected Thread thread;
 
-  public Maquina(int id) {
+  public Maquina(int id, int tipo) {
     this.id = id;
+    this.tipo = tipo;
     this.ligada = false;
     this.emFuncionamento = false;
   }
+
+  public void iniciarThread(){
+    if (thread == null || !thread.isAlive()) {
+      thread = new Thread(new Runnable() {
+        public void run() {
+          // Lógica de execução da máquina
+          while (ligada && emFuncionamento) {
+            
+          }
+        }
+      });
+      thread.start();
+    }
+  }
+
+  public void pararThread(){
+    if (thread != null && thread.isAlive()) {
+      emFuncionamento = false;
+      try {
+        thread.join(); // Aguarda a thread terminar
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+  } 
 
   public void ligar() {
     this.ligada = true;
@@ -26,6 +54,7 @@ class Maquina extends Thread {
     if (this.emFuncionamento == true){
       System.out.println("Máquina de ID: " + id + ", parou de trabalhar.");
       this.emFuncionamento = false;
+      pararThread();
     }else{
       System.out.println("A Máquina não está trabalhando.");
     }
@@ -34,30 +63,82 @@ class Maquina extends Thread {
 
 // Máquina de produção
 class MaquinaProducao extends Maquina {
-  public MaquinaProducao(int id) {
-    super(id);
+
+  public MaquinaProducao(int id, int tipo) {
+    super(id,tipo);
+  }
+
+  @Override
+  public void iniciarThread(){
+    if (thread == null || !thread.isAlive()) {
+      thread = new Thread(new Runnable() {
+        public void run() {
+          // Lógica de execução da máquina
+          while (ligada && emFuncionamento) {
+            try {
+              switch(tipo){
+                case 0: Thread.sleep(20000); break; // Padrão
+                case 1: Thread.sleep(25000); break; // Tecidos
+                case 2: Thread.sleep(30000); break; // Alimentos
+                case 3: Thread.sleep(40000); break; // Veículos
+                case 4: Thread.sleep(35000); break; // Móveis
+                case 5: Thread.sleep(45000); break; // Eletrônicos
+                case 6: Thread.sleep(25000); break; // Brinquedos
+              }
+            } catch (InterruptedException e) {
+              e.printStackTrace();
+            }
+          }
+        }
+      });
+      thread.start();
+    }
   }
 
   public void produzir() {
     if (ligada) {
       this.emFuncionamento = true;
       System.out.println("\nMáquina de produção com ID: " + id + ", está produzindo.");
+      iniciarThread();
     } else {
       System.out.println("\nA máquina de produção com ID: " + id + ", está desligada. Não é possível produzir.");
+    }
+  }
+
+  public void produzirProduto(){
+    if (emFuncionamento) {
+
     }
   }
 }
 
 // Máquina de embalagem
 class MaquinaEmbalagem extends Maquina {
-  public MaquinaEmbalagem(int id) {
-    super(id);
+
+  public MaquinaEmbalagem(int id, int tipo) {
+    super(id,tipo);
+  }
+
+  @Override
+  public void iniciarThread(){
+    if (thread == null || !thread.isAlive()) {
+      thread = new Thread(new Runnable() {
+        public void run() {
+          // Lógica de execução da máquina
+          while (ligada && emFuncionamento) {
+            
+          }
+        }
+      });
+      thread.start();
+    }
   }
 
   public void embalar() {
     if(ligada) {
       this.emFuncionamento = true;
       System.out.println("\nMáquina de embalagem com ID: " + id + ", está embalando.");
+      iniciarThread();
     } else {
       System.out.println("\nA máquina de embalagem com ID: " + id + ", está desligada. Não é possível embalar.");
     }
@@ -66,14 +147,29 @@ class MaquinaEmbalagem extends Maquina {
 
 // Máquina de inspeção
 class MaquinaInspecao extends Maquina {
-  public MaquinaInspecao(int id) {
-    super(id);
+  public MaquinaInspecao(int id, int tipo) {
+    super(id,tipo);
+  }
+  @Override
+  public void iniciarThread(){
+    if (thread == null || !thread.isAlive()) {
+      thread = new Thread(new Runnable() {
+        public void run() {
+          // Lógica de execução da máquina
+          while (ligada && emFuncionamento) {
+            
+          }
+        }
+      });
+      thread.start();
+    }
   }
 
   public void inspecionar() {
     if(ligada) {
       this.emFuncionamento = true;
       System.out.println("\nMáquina de inspeção com ID: " + id + ", está inspecionando.");
+      iniciarThread();
     }else{
       System.out.println("\nA máquina de inspeção com ID: " + id + ", está desligada. Não é possível inspecionar.");
     }
