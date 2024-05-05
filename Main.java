8dc62a234dbcb4437b487f2997a54d0da8c6c141
Gabriel <70419113+GabriBerp@users.noreply.github.com>
2024-05-04 21:03:41 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -42,6 +42,7 @@ public class Main {
     int qnt_producao = 0;
     int qnt_embalagem = 0;
     int qnt_inspecao = 0;
+    int qnt_entrega = 0;
 
     int op = 0;
     int id = 0;
@@ -62,13 +63,14 @@ public class Main {
                 qnt_producao = 0;
                 qnt_embalagem = 0;
                 qnt_inspecao = 0;
+                qnt_entrega = 0;
                 // Adiciona X maquinas a um ArrayList
                 String numMaquinasInput = JOptionPane.showInputDialog("== Iniciando Fabrica " + fab_id + " ==\n\nQuantas máquinas vão ser automatizadas? ");
                 int numMaquinas = Integer.parseInt(numMaquinasInput);
                 System.out.println(numMaquinas + " maquinas selecionadas.");
                 // Loop para criar as máquinas
                 for (int i = 1; i <= numMaquinas; i++) {
-                    String tipoInput = JOptionPane.showInputDialog("Tipo de máquina para a máquina " + i + ":\n1. Produção\n2. Embalagem\n3. Inspeção\nEscolha o tipo (1, 2 ou 3): ");
+                    String tipoInput = JOptionPane.showInputDialog("Tipo de máquina para a máquina " + i + ":\n1. Produção\n2. Embalagem\n3. Inspeção\n4. Entrega\nEscolha o tipo (1, 2, 3 ou 4): ");
                     int tipo = Integer.parseInt(tipoInput);
                     switch (tipo) {
                     case 1:
@@ -84,6 +86,11 @@ public class Main {
                     case 3:
                         automacao.adicionarMaquina(new MaquinaInspecao(i,automacao.tipo));
                         qnt_inspecao++;
+                        tipo = 0;
+                        break;
+                    case 4:
+                        automacao.adicionarMaquina(new MaquinaEntrega(i,automacao.tipo));
+                        qnt_entrega++;
                         tipo = 0;
                         break;
                     default:
@@ -103,40 +110,41 @@ public class Main {
                     maquinasInfo.append("Máquina de Embalagem, ID: " + maquina.id + "\n");
                     } else if (maquina instanceof MaquinaInspecao) {
                     maquinasInfo.append("Máquina de Inspeção, ID: " + maquina.id + "\n");
-                    }
+                    } else if (maquina instanceof MaquinaEntrega) {
+                      maquinasInfo.append("Máquina de Entrega, ID: " + maquina.id + "\n");
+                      }
                 }
                 maquinasInfo.append("\nQuantidade de máquinas por tipo:\nProdução: " + qnt_producao + "\nEmbalagem: "
-                    + qnt_embalagem + "\nInspeção: " + qnt_inspecao);
+                    + qnt_embalagem + "\nInspeção: " + qnt_inspecao + "\nEntrega: " + qnt_entrega);
                 JOptionPane.showMessageDialog(null, maquinasInfo.toString());
                 automacao.inicializar();
             }
-            
             do {
-            maquinas = automacao.PegarMaquinas();
-            switch (automacao.tipo) {
-                case 1:
-                op = Integer.parseInt(JOptionPane.showInputDialog("\n\nMenu de Automação de Máquinas:\n===\nFabrica Padrão\nID: " + fab_id + "\n===\n1. Ligar Máquinas\n2. Desligar Máquinas\n3. Listar Máquinas\n4. Automatizar Máquinas\n5. Parar Automatização\n6. Voltar\nEscolha uma opção (1, 2, 3, 4, 5 ou 6): "));
-                break;
-                case 2:
-                op = Integer.parseInt(JOptionPane.showInputDialog("\n\nMenu de Automação de Máquinas:\n===\nFabrica de Tecidos\nID: " + fab_id + "\n===\n1. Ligar Máquinas\n2. Desligar Máquinas\n3. Listar Máquinas\n4. Automatizar Máquinas\n5. Parar Automatização\n6. Voltar\nEscolha uma opção (1, 2, 3, 4, 5 ou 6): "));
-                break;
-                case 3:
-                op = Integer.parseInt(JOptionPane.showInputDialog("\n\nMenu de Automação de Máquinas:\n===\nFabrica de Alimentos\nID: " + fab_id + "\n===\n1. Ligar Máquinas\n2. Desligar Máquinas\n3. Listar Máquinas\n4. Automatizar Máquinas\n5. Parar Automatização\n6. Voltar\nEscolha uma opção (1, 2, 3, 4, 5 ou 6): "));
-                break;
-                case 4:
-                op = Integer.parseInt(JOptionPane.showInputDialog("\n\nMenu de Automação de Máquinas:\n===\nFabrica de Veiculos\nID: " + fab_id + "\n===\n1. Ligar Máquinas\n2. Desligar Máquinas\n3. Listar Máquinas\n4. Automatizar Máquinas\n5. Parar Automatização\n6. Voltar\nEscolha uma opção (1, 2, 3, 4, 5 ou 6): "));
-                break;
-                case 5:
-                op = Integer.parseInt(JOptionPane.showInputDialog("\n\nMenu de Automação de Máquinas:\n===\nFabrica de Moveis\nID: " + fab_id + "\n===\n1. Ligar Máquinas\n2. Desligar Máquinas\n3. Listar Máquinas\n4. Automatizar Máquinas\n5. Parar Automatização\n6. Voltar\nEscolha uma opção (1, 2, 3, 4, 5 ou 6): "));
-                break;
-                case 6:
-                op = Integer.parseInt(JOptionPane.showInputDialog("\n\nMenu de Automação de Máquinas:\n===\nFabrica de Eletronicos\nID: " + fab_id + "\n===\n1. Ligar Máquinas\n2. Desligar Máquinas\n3. Listar Máquinas\n4. Automatizar Máquinas\n5. Parar Automatização\n6. Voltar\nEscolha uma opção (1, 2, 3, 4, 5 ou 6): "));
-                break;
-                case 7:
-                op = Integer.parseInt(JOptionPane.showInputDialog("\n\nMenu de Automação de Máquinas:\n===\nFabrica de Brinquedos\nID: " + fab_id + "\n===\n1. Ligar Máquinas\n2. Desligar Máquinas\n3. Listar Máquinas\n4. Automatizar Máquinas\n5. Parar Automatização\n6. Voltar\nEscolha uma opção (1, 2, 3, 4, 5 ou 6): "));
-                break;
-            }
-            switch (op) {
+              maquinas = automacao.PegarMaquinas();
+              switch (automacao.tipo) {
+                  case 1:
+                  op = Integer.parseInt(JOptionPane.showInputDialog("\n\nMenu de Automação de Máquinas:\n===\nFabrica Padrão\nID: " + fab_id + "\n===\n1. Ligar Máquinas\n2. Desligar Máquinas\n3. Listar Máquinas\n4. Automatizar Máquinas\n5. Parar Automatização\n6. Voltar\nEscolha uma opção (1, 2, 3, 4, 5 ou 6): "));
+                  break;
+                  case 2:
+                  op = Integer.parseInt(JOptionPane.showInputDialog("\n\nMenu de Automação de Máquinas:\n===\nFabrica de Tecidos\nID: " + fab_id + "\n===\n1. Ligar Máquinas\n2. Desligar Máquinas\n3. Listar Máquinas\n4. Automatizar Máquinas\n5. Parar Automatização\n6. Voltar\nEscolha uma opção (1, 2, 3, 4, 5 ou 6): "));
+                  break;
+                  case 3:
+                  op = Integer.parseInt(JOptionPane.showInputDialog("\n\nMenu de Automação de Máquinas:\n===\nFabrica de Alimentos\nID: " + fab_id + "\n===\n1. Ligar Máquinas\n2. Desligar Máquinas\n3. Listar Máquinas\n4. Automatizar Máquinas\n5. Parar Automatização\n6. Voltar\nEscolha uma opção (1, 2, 3, 4, 5 ou 6): "));
+                  break;
+                  case 4:
+                  op = Integer.parseInt(JOptionPane.showInputDialog("\n\nMenu de Automação de Máquinas:\n===\nFabrica de Veiculos\nID: " + fab_id + "\n===\n1. Ligar Máquinas\n2. Desligar Máquinas\n3. Listar Máquinas\n4. Automatizar Máquinas\n5. Parar Automatização\n6. Voltar\nEscolha uma opção (1, 2, 3, 4, 5 ou 6): "));
+                  break;
+                  case 5:
+                  op = Integer.parseInt(JOptionPane.showInputDialog("\n\nMenu de Automação de Máquinas:\n===\nFabrica de Moveis\nID: " + fab_id + "\n===\n1. Ligar Máquinas\n2. Desligar Máquinas\n3. Listar Máquinas\n4. Automatizar Máquinas\n5. Parar Automatização\n6. Voltar\nEscolha uma opção (1, 2, 3, 4, 5 ou 6): "));
+                  break;
+                  case 6:
+                  op = Integer.parseInt(JOptionPane.showInputDialog("\n\nMenu de Automação de Máquinas:\n===\nFabrica de Eletronicos\nID: " + fab_id + "\n===\n1. Ligar Máquinas\n2. Desligar Máquinas\n3. Listar Máquinas\n4. Automatizar Máquinas\n5. Parar Automatização\n6. Voltar\nEscolha uma opção (1, 2, 3, 4, 5 ou 6): "));
+                  break;
+                  case 7:
+                  op = Integer.parseInt(JOptionPane.showInputDialog("\n\nMenu de Automação de Máquinas:\n===\nFabrica de Brinquedos\nID: " + fab_id + "\n===\n1. Ligar Máquinas\n2. Desligar Máquinas\n3. Listar Máquinas\n4. Automatizar Máquinas\n5. Parar Automatização\n6. Voltar\nEscolha uma opção (1, 2, 3, 4, 5 ou 6): "));
+                  break;
+              }
+              switch (op) {
                 case 1:
                     opcao = Integer.parseInt(JOptionPane.showInputDialog("Deseja ligar uma maquina especifica, ou todas? (1- Especifica, 2- Todas)"));
                     if (opcao == 1) {
@@ -182,6 +190,8 @@ public class Main {
                             message += "\n\nMáquina de Embalagem " + maquina.id;
                             } else if (maquina instanceof MaquinaInspecao) {
                             message += "\n\nMáquina de Inspeção " + maquina.id;
+                            } else if (maquina instanceof MaquinaEntrega) {
+                              message += "Máquina de Entrega " + maquina.id + "\n";
                             }
                             if (maquina.ligada) {
                             message += "\nLigada: Sim";
@@ -207,6 +217,8 @@ public class Main {
                             message += "Máquina de Embalagem " + maquina.id + "\n";
                         } else if (maquina instanceof MaquinaInspecao) {
                             message += "Máquina de Inspeção " + maquina.id + "\n";
+                        } else if (maquina instanceof MaquinaEntrega) {
+                          message += "Máquina de Entrega " + maquina.id + "\n";
                         }
                         if (maquina.ligada) {
                             message += "Ligada: Sim\n";
@@ -237,6 +249,8 @@ public class Main {
                             ((MaquinaEmbalagem) maquina).embalar();
                           } else if (maquina instanceof MaquinaInspecao) {
                             ((MaquinaInspecao) maquina).inspecionar();
+                          } else if (maquina instanceof MaquinaEntrega) {
+                            ((MaquinaEntrega) maquina).entregar();
                           }
                           break;
                         } else if ((maquina.id == id) && (maquina.emFuncionamento)) {
@@ -253,6 +267,8 @@ public class Main {
                             ((MaquinaEmbalagem) maquina).embalar();
                           } else if (maquina instanceof MaquinaInspecao) {
                             ((MaquinaInspecao) maquina).inspecionar();
+                          } else if (maquina instanceof MaquinaEntrega) {
+                            ((MaquinaEntrega) maquina).entregar();
                           }
                         }
                       }
@@ -279,7 +295,7 @@ public class Main {
                       JOptionPane.showMessageDialog(null, "Opção inválida.");
                     }
                     break;
-            }
+              }
             }while(op!=6);
             break;
         case 2:
@@ -325,6 +341,8 @@ public class Main {
                     fabricasInfo.append("  Máquina de Embalagem ").append(maquina.id).append("\n");
                   } else if (maquina instanceof MaquinaInspecao) {
                     fabricasInfo.append("  Máquina de Inspeção ").append(maquina.id).append("\n");
+                  } else if (maquina instanceof MaquinaEntrega) {
+                    fabricasInfo.append("  Máquina de Entrega ").append(maquina.id).append("\n");
                   }
                   fabricasInfo.append(maquina.ligada ? "  Ligada: Sim\n" : "  Ligada: Não\n");
                   fabricasInfo.append(maquina.emFuncionamento ? "  Em Funcionamento: Sim\n\n" : "  Em Funcionamento: Não\n\n");
