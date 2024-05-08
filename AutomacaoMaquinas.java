b5fbc562f8774c21286c27f85c3e397dd95805b7
Gabriel (GabriBerp) <oviciado65@gmail.com>
2024-05-08 08:35:02 -0300

    Alterado algumas coisas bobas

--- AutomacaoMaquinas.java
+++ AutomacaoMaquinas.java
@@ -2,130 +2,82 @@ import java.util.ArrayList;
 import java.lang.Thread;
 
 class AutomacaoMaquinas extends Thread {
-  private ArrayList<Maquina> maquinas;
-  private boolean inicializada;
-  public int tipo;
-  public int id;
-  private int produtosProduzidos;
-  private int produtosEmbalados;
-  private int produtosInspecionados;
-  private int produtosEntregues;
-  protected Thread thread;
-  private Monitoramento monitor;
+    private ArrayList<Maquina> maquinas;
+    private boolean inicializada;
+    public int tipo;
+    public int id;
+    private int produtosProduzidos;
+    private int produtosEmbalados;
+    private int produtosInspecionados;
+    private int produtosEntregues;
+    protected Thread thread;
 
-  public int getProdutosProduzidos() {
-    return produtosProduzidos;
-  }
+    public int getProdutosProduzidos() {
+        return produtosProduzidos;
+    }
 
-  public int getProdutosEmbalados() {
-      return produtosEmbalados;
-  }
+    public int getProdutosEmbalados() {
+        return produtosEmbalados;
+    }
 
-  public int getProdutosInspecionados() {
-      return produtosInspecionados;
-  }
+    public int getProdutosInspecionados() {
+        return produtosInspecionados;
+    }
 
-  public int getProdutosEntregues() {
-      return produtosEntregues;
-  }
+    public int getProdutosEntregues() {
+        return produtosEntregues;
+    }
 
-  public synchronized void atualizarProdutosProduzidos(int quantidade) {
-    produtosProduzidos += quantidade;
-  }
+    public synchronized void atualizarProdutosProduzidos(int quantidade) {
+        produtosProduzidos += quantidade;
+    }
 
-  public synchronized void atualizarProdutosEmbalados(int quantidade) {
-      produtosEmbalados += quantidade;
-  }
+    public synchronized void atualizarProdutosEmbalados(int quantidade) {
+        produtosEmbalados += quantidade;
+    }
 
-  public synchronized void atualizarProdutosInspecionados(int quantidade) {
-      produtosInspecionados += quantidade;
-  }
+    public synchronized void atualizarProdutosInspecionados(int quantidade) {
+        produtosInspecionados += quantidade;
+    }
 
-  public synchronized void atualizarProdutosEntregues(int quantidade) {
-      produtosEntregues += quantidade;
-  }
+    public synchronized void atualizarProdutosEntregues(int quantidade) {
+        produtosEntregues += quantidade;
+    }
 
-  Thread dataUpdateThread = new Thread(() -> {
-    while (true) {
-        int production = getProdutosProduzidos();
-        int packaging = getProdutosEmbalados();
-        int inspection = getProdutosInspecionados();
-        int delivery = getProdutosEntregues();
+    public AutomacaoMaquinas(int id) {
+        this.maquinas = new ArrayList<Maquina>();
+        this.inicializada = false;
+        this.id = id;
+    }
 
-        // Atualize os dados na janela de visualização em tempo real
-        monitor.updateData(production, packaging, inspection, delivery);
+    public boolean foiInicializada() {
+        return this.inicializada;
+    }
 
-        // Espere um pouco antes de atualizar novamente
-        try {
-            Thread.sleep(1000); // Espere 1 segundo (1000 milissegundos)
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+    public void inicializar() {
+        this.inicializada = true;
+    }
+
+    public ArrayList<Maquina> PegarMaquinas() {
+        return this.maquinas;
+    }
+
+    public void adicionarMaquina(Maquina maquina) {
+        maquinas.add(maquina);
+        System.out.println("\nMáquina com ID: " + maquina.id + ", adicionada.\n");
+    }
+
+    public void ligarTodasMaquinas() {
+        for (Maquina maquina : maquinas) {
+            maquina.ligar();
         }
+        System.out.println("\nTodas as máquinas foram ligadas.");
     }
-});
 
-  public AutomacaoMaquinas(int id) {
-    this.maquinas = new ArrayList<Maquina>();
-    this.inicializada = false;
-    this.id = id;
-    this.monitor = new Monitoramento();
-    iniciarThread();
-  }
-
-  public int iniciarMonitoramento() {
-    for (Maquina maquina : maquinas) {
-        if (maquina.emFuncionamento) {
-            monitor.setVisible(true); // Mostra a janela de monitoramento
-            monitor.updateData(getProdutosProduzidos(), getProdutosEmbalados(), getProdutosInspecionados(), getProdutosEntregues());
-            return 1;
+    public void desligarTodasMaquinas() {
+        for (Maquina maquina : maquinas) {
+            maquina.desligar();
         }
+        System.out.println("\nTodas as máquinas foram desligadas.");
     }
-    monitor.setVisible(false); // Esconde a janela de monitoramento se nenhuma máquina estiver em funcionamento
-    return 0;
-}
-
-  public void iniciarThread(){
-    if (thread == null || !thread.isAlive()) {
-      thread = new Thread(new Runnable() {
-        public void run() {
-          // Lógica de execução da máquina
-          while (true) {
-              iniciarMonitoramento();
-          }
-        }
-      });
-      thread.start();
-    }
-  }
-
-  public boolean foiInicializada(){
-    return this.inicializada;
-  }
-
-  public void inicializar(){
-    this.inicializada = true;
-  }
-
-  public ArrayList<Maquina> PegarMaquinas(){
-    return this.maquinas;
-  }
-
-  public void adicionarMaquina(Maquina maquina) {
-    maquinas.add(maquina);
-    System.out.println("\nMáquina com ID: " + maquina.id + ", adicionada.\n");
-  }
-
-  public void ligarTodasMaquinas() {
-    for (Maquina maquina : maquinas) {
-      maquina.ligar();
-    }
-    System.out.println("\nTodas as máquinas foram ligadas.");
-  }
-
-  public void desligarTodasMaquinas() {
-    for (Maquina maquina : maquinas) {
-      maquina.desligar();
-    }
-    System.out.println("\nTodas as máquinas foram desligadas.");
-  }
 }
